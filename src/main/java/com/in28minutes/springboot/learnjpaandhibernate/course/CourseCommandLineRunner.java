6625d544b1bcee62a772dec51dd2c157f471e832
omaritocen <omaralaaabdelkader@gmail.com>
2023-07-29 19:55:56 +0300

    Add Spring Data JPA repository

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -1,28 +1,30 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 
-    private final CourseJpaRepository repository;
+//    @Autowired
+//    private final CourseJpaRepository repository;
 
-    public CourseCommandLineRunner(CourseJpaRepository repository) {
-        this.repository = repository;
-    }
 
+    @Autowired
+    private  CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS", "in28minutes"));
-        repository.insert(new Course(2, "Learn DevOps", "in28minutes"));
-        repository.insert(new Course(3, "Learn Azure", "in28minutes"));
+        repository.save(new Course(1, "Learn AWS", "in28minutes"));
+        repository.save(new Course(2, "Learn DevOps", "in28minutes"));
+        repository.save(new Course(3, "Learn Azure", "in28minutes"));
 
-        repository.deleteById(2);
+        repository.deleteById(2L);
 
-        System.out.println(repository.findById(3));
-        System.out.println(repository.findById(1));
+        System.out.println(repository.findById(3L));
+        System.out.println(repository.findById(1L));
     }
 }
